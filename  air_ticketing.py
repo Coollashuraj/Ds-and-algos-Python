f767b3fe869e9cd158dec28552e64e2a012621b6
Maha <coollashuraj@gmail.com>
2020-07-29 22:57:01 -0700

    Initial Comment

---
+++  air_ticketing.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+class Flight:
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+
+    def open_seats(self):
+      return self.capacity - len(self.passengers)
+""
+
+flight = Flight(3)
+
+people = ['Bob', 'Micky', 'Larry', 'pat']
+for person in people:
+    success = flight.add_passenger(person)
+    if success:
+        print ("Added" + " " + person + " to the flight successfully. ")
+    else:
+        print ("No available seats for" + " " +person)
